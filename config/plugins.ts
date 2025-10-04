84a88d9b1e4fe89378c5f2fd77b4cbb6830cccae
Vladyslav Bondarchuk <vladyslav_bondarchuk@epam.com>
2025-10-05 01:13:54 +0300

    Prepare basic content types

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,16 @@
-export default () => ({});
+export default () => ({
+  navigation: {
+    enabled: true,
+    config: {
+      contentTypes: ["api::page.article"],
+      defaultContentTypes: "api::page.article",
+      contentTypesNameFields: {
+        "api::page.article": ["title"],
+      },
+      pathDefaultFields: {
+        "api::page.article": ["slug"],
+      },
+      allowedLevels: 2,
+    },
+  },
+});
